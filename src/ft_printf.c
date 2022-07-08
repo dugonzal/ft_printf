65470eac79a081d18d8fde0a19ee7a2ad61b2f4a
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-07-08 18:31:10 +0200

    sd

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,19 +6,40 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 11:31:15 by dugonzal          #+#    #+#             */
-/*   Updated: 2022/07/07 14:19:37 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/07/08 16:53:21 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/printf.h"
 
+/*
 int	ft_printf(char const *format, ...)
 {
-}
+	va_list		list;	char const	*format;
 
+	va_start(list, format);
+	while (*format != 0)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+				ft_putchar('%');
+			else
+				ft_printf_parse(format, &i, list);
+		}
+		else
+			ft_putchar(*format);
+		format++;
+	}
+	va_end(list);
+	return (0);
+}
+*/
 int	main(void)
 {
-	char const	*format;
+	ft_printf("%s", "hello");
+	return (0);
 }
 
 /*
