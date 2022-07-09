5712637702e7305f9ddb8d68d8130b36ff3d887d
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-07-09 16:17:08 +0200

    make rules major

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 15:13:20 by ciclo             #+#    #+#             */
-/*   Updated: 2022/07/09 15:55:29 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/07/09 16:06:10 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,15 @@
 
 int	ft_printf(char const *format, ...)
 {
-	va_list		list;	char const	*format;
-
-	va_start(list, format);
-	while (*format != 0)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '%')
-				ft_putchar('%');
-			else
-				ft_printf_parse(format, &i, list);
-		}
-		else
-			ft_putchar(*format);
-		format++;
-	}
-	va_end(list);
-	return (0);
 }
 
 int	main(void)
 {
+	printf ("main: here\n");
 	return (0);
 }
 
 /*
- conversiones a implementar : cspdiuxX %.
+ conversiones a implementar : c,s,p,d,i,u,x,X,%.
+ funciones autorizadas malloc, free, write, va_start, va_arg, va_copy, va_end
 */
